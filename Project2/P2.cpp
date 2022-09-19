0127028b85a5ed1681a545fd0f2d6ce2fdc1ea8b
Daniel <daniel.lima@inmetrics.com.br>
2022-09-19 19:43:24 +0200

    P2

--- Project2/P2.cpp
+++ Project2/P2.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-    int N = 6;
-    int n = N + 1;
+    int N = 6;     // dim of square matrix
+    int n = N + 1; // steps
     double h = 1. / n;
     // initialising v that I will be solving for
     // giving it a dummy first index
@@ -14,22 +14,26 @@ int main()
     v(0) = 0.;
     // std::cout << v_star;
     double a = -1 / (h * h);
-    double b = 2 / (h * h);
+    double d = 2 / (h * h);
+
     arma::mat A = arma::mat(N, N).fill(0.);
-    A.diag() = arma::vec(N).fill(b);
+    // sets up the tridiagonal
+    A.diag() = arma::vec(N).fill(d);
     A.diag(1) = arma::vec(N - 1).fill(a);
     A.diag(-1) = arma::vec(N - 1).fill(a);
-    arma::vec eigval;
-    arma::mat eigvec;
-    eig_sym(eigval, eigvec, A);
-    // scaling of eigenvectors is not fixed yet; gives some
-    // not important yet, only needed for presentation at the end
-    arma::vec norm_eigvec = normalise(eigvec);
-    std::cout << norm_eigvec << '\n';
 
-    int check_col = 3;
-    arma::vec check1 = A * eigvec.col(check_col);
-    arma::vec check2 = eigval(check_col) * eigvec.col(check_col);
-    arma::vec final = check1 - check2;
-    // std::cout << final << '\n';
+    arma::vec eigval; // vector of eigenvalues
+    arma::mat eigvec; // matrix of eigenvectors
+    eig_sym(eigval, eigvec, A);
+
+    // not important yet, only needed for presentation at the end
+    arma::mat norm_eigvec = normalise(eigvec); // normalized matrix of eigenvectors
+
+    // realize we can check all at once
+    for (int i = 0; i < N; i++)
+    {
+        arma::mat left_side = A * eigvec.col(i);
+        arma::mat rigt_side = eigval(i) * eigvec.col(i);
+        std::cout << left_side - rigt_side << '\n';
+    }
 }
