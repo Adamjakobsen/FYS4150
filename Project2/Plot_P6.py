85b0d322b0e5e46279591406afe3afd1fa308fe9
Adamjakobsen <adam.r.jakobsen@gmail.com>
2022-09-25 13:56:05 +0200

    smol update

--- Project2/Plot_P6.py
+++ Project2/Plot_P6.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 #Get data from P6eigenboys.txt, first column is v1,second column is v2, third column is v3
 
 data = np.loadtxt("P6eigenboys.txt",float)
-x = np.linspace(0,1,12)
+
 v1 = data[:,0]
 v2 = data[:,1]
 v3 = data[:,2]
@@ -17,6 +17,7 @@ v1 = np.append(v1,0)
 v2 = np.append(v2,0)
 v3 = np.append(v3,0)
 
+x = np.linspace(0,1,len(v1))
 #Plot the data
 plt.plot(x,v1,label="v1")
 plt.plot(x,v2,label="v2")
@@ -24,5 +25,5 @@ plt.plot(x,v3,label="v3")
 plt.xlabel("x")
 plt.ylabel("v")
 plt.legend()
-plt.savefig('P6_fig.pdf')
+plt.savefig('P6_fig'+f'_{len(x)}'+ '.pdf')
 plt.show()
