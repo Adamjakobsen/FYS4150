492b631fc597873180d023294c27b32636641597
Adamjakobsen <adam.r.jakobsen@gmail.com>
2022-09-25 16:01:52 +0200

    Updated P5 plotting script

--- Project2/Plot_P5.py
+++ Project2/Plot_P5.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-print("Working dir:", os.getcwd())
+
 
 # inspired by code for reading file https://stackoverflow.com/questions/38532298/how-can-you-plot-data-from-a-txt-file-using-matplotlib
 
-data1 = np.loadtxt("NOperationsP5Dense.txt")
-data2 = np.loadtxt("NOperationsP5Tridiag.txt")
+data1 = np.loadtxt('NOperationsP5Dense.txt ', float)
+data2 = np.loadtxt('NOperationsP5Tridiag.txt ',float)
 
 x = np.zeros(len(data1))
 y1 = np.zeros(len(data1))
@@ -18,9 +18,13 @@ for i in range(len(data1)):
     y2[i] = data2[i][1]
 
 
-plt.plot(x, y1)
-plt.plot(x, y2)
+plt.plot(x, y1,label='Dense')
+plt.plot(x, y2,label='Tridiagonal')
 
 plt.xlabel("x")
 plt.ylabel("Number of operations")
+plt.yscale('log')
+plt.xscale('log')
+plt.legend()
+plt.savefig('P5_fig.pdf')
 plt.show()
