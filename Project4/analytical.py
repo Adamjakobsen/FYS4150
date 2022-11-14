2d567c050a8b208b494d757d2eda8326d42af65a
Nigar Abbasova <87075298+nigarabbasova@users.noreply.github.com>
2022-11-14 23:38:35 +0100

    added relative error calcs

--- Project4/analytical.py
+++ Project4/analytical.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import random
 
 #define constants 
-T = 2.4 #J/k_B
+T = 1 #J/k_B
 J = 1
 k_B = 1
 N = 4
@@ -76,21 +76,66 @@ def specific_heat_cap():
 #normalised susceptibility 
 def susceptibility():
     prefactor = 1/N * (1/(k_B*T))
-    magnet_diff = exp_M2 - (exp_M)**2
+    magnet_diff = exp_M2 - (abs(exp_M))**2
     chi = prefactor*magnet_diff
     return chi
 
-C_v = specific_heat_cap()
-chi = susceptibility()
+C_v_analyt = specific_heat_cap()
+chi_analyt = susceptibility()
 
-data = np.array([exp_E_N, exp_E2_N, exp_M_N, exp_M2_N, C_v, chi])
+data = np.array([exp_E_N, exp_M_N, C_v_analyt, chi_analyt])
 
 #hacky way of saving 1D numpy array as a row and not a column
 file = np.savetxt('analytical_values_L_2.txt', [data], delimiter = ' ')
 
+"""Computing relative errors to compare analytical with numerical """
 
-data_read = np.loadtxt('analytical_values_L_2.txt')
-#print out like this: 
-# print(data_read[1])
+data_sim3 = np.loadtxt('quantities_L_2_mc_1000.txt') #output: avg_e << " " << avg_mabs << " " << Cv << " " << chi
+# data_sim6 = np.loadtxt('quantities_L_2_mc_1000000.txt')
+# data_sim8 = np.loadtxt('quantities_L_2_mc_100000000.txt')
 
+#extract last values from the simulated data file to get the most accurate MC cycle output 
+def relative_error(data):
+    exp_E_N_mc = data[-1][0]
+    exp_M_N_mc = data[-1][1]
+    C_v_mc = data[-1][2]
+    chi_mc = data[-1][3]
+    
+    delta_exp_E_N3 = abs((exp_E_N_mc - exp_E_N)/exp_E_N)
+    delta_exp_M_N3 = abs((exp_M_N_mc - exp_M_N)/exp_M_N)
+    delta_C_v3 = abs((C_v_mc - C_v_analyt)/C_v_analyt)
+    delta_chi3 = abs((chi_mc - chi_analyt)/chi_analyt)
+    
+    return(delta_exp_E_N3, delta_exp_M_N3, delta_C_v3, delta_chi3)
+    
+mc_3 = relative_error(data_sim3)
+# mc_6 = relative_error(data_sim6)
+# mc_8 = relative_error(data_sim8)
+
+print("Relative errors for MC = 10^3")
+print("Relative error of exp. energy per spin = ", mc_3[0])
+print("Relative error of exp. magnetisation per spin = " , mc_3[1])
+print("Relative error of exp. C_v = ", mc_3[2])
+print("Relative error of exp. chi = ", mc_3[3])
+
+
+# print("Relative errors for MC = 10^6")
+# print("Relative error of exp. energy per spin = ", mc_3[0])
+# print("Relative error of exp. magnetisation per spin = " , mc_3[1])
+# print("Relative error of exp. C_v = ", mc_3[2])
+# print("Relative error of exp. chi = ", mc_3[3])
+
+# print("Relative errors for MC = 10^8")
+# print("Relative error of exp. energy per spin = ", mc_3[0])
+# print("Relative error of exp. magnetisation per spin = " , mc_3[1])
+# print("Relative error of exp. C_v = ", mc_3[2])
+# print("Relative error of exp. chi = ", mc_3[3])
+
+# data_sim5 = np.loadtxt('quantities_L_2_mc_1000.txt')
+# mc_5 = relative_error(data_sim5)
+# print("Relative errors for MC = 10^5")
+# print("Relative error of exp. energy per spin = ", mc_3[0])
+# print("Relative error of exp. magnetisation per spin = " , mc_3[1])
+# print("Relative error of exp. C_v = ", mc_3[2])
+# print("Relative error of exp. chi = ", mc_3[3])
 
