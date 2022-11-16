c480bfa992a0aabb00d0e54a31e7248571e1eb63
Daniel <daniel.lima@inmetrics.com.br>
2022-11-16 01:46:13 +0100

    my dream is to fly over the rainbow so high

--- Project4/toymodel.cpp
+++ Project4/toymodel.cpp
@@ -9,21 +9,18 @@
 #include <vector>
 #include <stdlib.h> /* srand, rand */
 
-arma::mat init_random_config(int L, double &exp_E, double &exp_M);
+arma::mat init_random_config(int L, double &exp_E, double &exp_M, int align);
 arma::mat find_interacting_pairs(int index, arma::vec raveled_config, int L);
-arma::mat evolve(arma::mat config, double beta, double &exp_E, double &exp_M);
+arma::mat evolve(arma::mat config, double beta, double &exp_E, double &exp_M, arma::vec p_vec);
 int mt_random_int(int low, int high);
 double mt_random_float(int low, int high);
 
-void monte_carlo(int L, int mc_cycles);
-// std::random_device rd;
-std::mt19937 gen(123);
+void monte_carlo(int L, int mc_cycles, int burn_pct, double T, int align);
+std::random_device rd;
+std::mt19937 gen(rd()); // gen(2) for a seed of 2
 // define your physical system's variables here:
 
 double kb = 1.;
-double T = 2.4;
-double beta = 1. / (kb * T);
-
 int main(int argc, char *argv[])
 {
     // main will only evolve the system and output the results to a file in matrix form
@@ -33,10 +30,19 @@ int main(int argc, char *argv[])
     int L = atoi(argv[1]);
 
     int mc_cycles = atoi(argv[2]); // 1 = L^2 runs, 2 = 2*L^2 runs, etc
-    monte_carlo(L, mc_cycles);
+    int burn_pct = atoi(argv[3]);  // 10 = 10% burn in, 20 = 20% burn in, etc
+    double lower_temp = atof(argv[4]);
+    double upper_temp = atof(argv[5]);
+    double temp_step = atof(argv[6]);
+    int align = atoi(argv[7]); // 0 = random, 1 = aligned up (1), 2 = aligned down (-1)
+
+    for (double T = lower_temp; T <= upper_temp; T += temp_step)
+    {
+        monte_carlo(L, mc_cycles, burn_pct, T, align);
+    }
 }
 
-arma::mat init_random_config(int L, double &E, double &M)
+arma::mat init_random_config(int L, double &E, double &M, int align)
 {
     int N = L * L;
     arma::mat config = arma::zeros<arma::mat>(L, L);
@@ -45,7 +51,19 @@ arma::mat init_random_config(int L, double &E, double &M)
 
     for (int i = 0; i < N; i++)
     {
-        raveled_config(i) = 2 * mt_random_int(0, 1) - 1; // makes -1 or 1 with equal probability
+        if (align == 0)
+        {
+            raveled_config(i) = mt_random_int(0, 1) * 2 - 1; // makes -1 or 1 with equal probability
+        }
+        else if (align == 1)
+        {
+            raveled_config(i) = 1;
+        }
+        else if (align == 2)
+        {
+            raveled_config(i) = -1;
+        }
+
         M += raveled_config(i);
     }
     // notice that to find the initial energy, we need another loop
@@ -57,6 +75,9 @@ arma::mat init_random_config(int L, double &E, double &M)
             E += -(interacting_pairs(j, 0) * interacting_pairs(j, 1));
         }
     }
+    E = E / 2; // since we double count each interaction
+
+    std::cout << "Initial energy: " << E << std::endl;
     return arma::reshape(raveled_config, L, L);
 }
 
@@ -87,7 +108,7 @@ arma::mat find_interacting_pairs(int index, arma::vec raveled_config, int L)
     return interacting_pairs;
 }
 
-arma::mat evolve(arma::mat config, double beta, double &E, double &M)
+arma::mat evolve(arma::mat config, double beta, double &E, double &M, arma::vec p_vec)
 {
     int L = config.n_rows;
     arma::mat raveled_config = arma::reshape(config, L * L, 1);
@@ -107,23 +128,43 @@ arma::mat evolve(arma::mat config, double beta, double &E, double &M)
 
         delta_E = -2 * neigbours_energy; // because because delta_E = E_new - E_old = -((-a)*b) - (-(a*b)) = 2*(a*b) = -2*neigbours_energy
 
-        double p = exp(-beta * delta_E);
-        if (mt_random_float(0, 1) < p)
+        if (delta_E <= 0)
         {
             raveled_config(i) *= -1;
             E += delta_E;
             M += 2 * raveled_config(i); // factor of 2 because we are only changing one spin
         }
+        else
+        {
+            double p = std::exp(-beta * delta_E);
+            // double p = p_vec[(delta_E + 8) / 4]; // this did not make it faster
+
+            if (mt_random_float(0, 1) < p)
+            {
+                raveled_config(i) *= -1;
+                E += delta_E;
+                M += 2 * raveled_config(i); // factor of 2 because we are only changing one spin
+            }
+        }
     }
     return arma::reshape(raveled_config, L, L);
 }
 
-void monte_carlo(int L, int mc_cycles)
+void monte_carlo(int L, int mc_cycles, int burn_pct, double T, int align)
 {
+    double beta = 1. / (kb * T);
+    arma::vec p_vec = arma::zeros<arma::vec>(5);
+    arma::vec delta_e_vec = arma::vec(std::vector<double>{-8, -4, 0, 4, 8});
+    for (int i = 0; i < 5; i++)
+    {
+        p_vec(i) = std::exp(-beta * delta_e_vec(i));
+        // std::cout << "delta_e_vec(i)" << delta_e_vec(i) << std::endl;
+    }
     std::ofstream file1;
     std::ofstream file2;
-    std::string filename = "config_L_" + std::to_string(L) + "_mc_" + std::to_string(mc_cycles) + ".txt";
-    std::string filename2 = "quantities_L_" + std::to_string(L) + "_mc_" + std::to_string(mc_cycles) + ".txt";
+
+    std::string filename = "cfg_L" + std::to_string(L) + "_mc_" + std::to_string(mc_cycles) + "_burn_" + std::to_string(burn_pct) + "_t_" + std::to_string(int(T)) + ".txt";
+    std::string filename2 = "qt_L" + std::to_string(L) + "_mc_" + std::to_string(mc_cycles) + "_burn_" + std::to_string(burn_pct) + "_t_" + std::to_string(int(T)) + ".txt";
 
     file1.open(filename);
     file2.open(filename2);
@@ -166,53 +207,52 @@ void monte_carlo(int L, int mc_cycles)
 
     double Cv = 0;
     double chi = 0;
-    arma::mat config = init_random_config(L, E, M);
+    arma::mat config = init_random_config(L, E, M, align);
 
     double N = L * L;
     int time_steps = mc_cycles * N;
-    int burn_in = int(0 * mc_cycles);
-    std::cout << "burn in: " << burn_in << std::endl;
+    int burn_in = int((burn_pct / 100.) * time_steps);
+    std::cout << "burned mc_cycles: " << burn_in << std::endl;
 
     // std::cout << "time_steps = " << time_steps << std::endl;
     for (int i = 0; i < time_steps; i++)
     {
-        config = evolve(config, beta, E, M);
+        config = evolve(config, beta, E, M, p_vec);
         // the following qtd will be calculated in a dumb way just to be didatic but uses lots of flops
-        if (i >= burn_in)
+
+        cumul_E += E;
+        cumul_e = cumul_E / N;
+        cumul_E2 += E * E;
+        cumul_e2 = cumul_E2 / (N * N);
+        avg_e = cumul_e / (i + 1);
+        avg_e2 = cumul_e2 / (i + 1);
+        avg_E2 = cumul_E2 / (i + 1);
+        avg_E = cumul_E / (i + 1);
+        var_E = avg_E2 - avg_E * avg_E;
+
+        cumul_M += M;
+        cumul_m = cumul_M / N;
+        cumul_M2 += M * M;
+        cumul_m2 = cumul_M2 / (N * N);
+        cumul_Mabs += std::abs(M);
+        avg_Mabs = cumul_Mabs / (i + 1);
+
+        cumul_mabs = cumul_Mabs / N;
+        avg_mabs = cumul_mabs / (i + 1);
+        avg_m2 = cumul_m2 / (i + 1);
+        avg_M = cumul_M / (i + 1);
+        avg_M2 = cumul_M2 / (i + 1);
+        avg_Mabs2 = avg_Mabs * avg_Mabs;
+
+        var_M = avg_M2 - avg_Mabs2;
+        Cv = (var_E) / (N * kb * T * T);
+        chi = (var_M) / (N * kb * T);
+        if (i > burn_in)
         {
-            // std::cout << "i = " << i << std::endl;
-            cumul_E += E;
-            cumul_e = cumul_E / N;
-            cumul_E2 += E * E;
-            cumul_e2 = cumul_E2 / (N * N);
-            avg_e = cumul_e / (i + 1 - burn_in);
-            avg_e2 = cumul_e2 / (i + 1 - burn_in);
-            avg_E2 = cumul_E2 / (i + 1 - burn_in);
-            avg_E = cumul_E / (i + 1 - burn_in);
-            var_E = avg_E2 - avg_E * avg_E;
-
-            cumul_M += M;
-            cumul_m = cumul_M / N;
-            cumul_M2 += M * M;
-            cumul_m2 = cumul_M2 / (N * N);
-            cumul_Mabs += std::abs(M);
-            avg_Mabs = cumul_Mabs / (i + 1 - burn_in);
-
-            cumul_mabs = cumul_Mabs / N;
-            avg_mabs = cumul_mabs / (i + 1 - burn_in);
-            avg_m2 = cumul_m2 / (i + 1 - burn_in);
-            avg_M = cumul_M / (i + 1 - burn_in);
-            avg_M2 = cumul_M2 / (i + 1 - burn_in);
-            avg_Mabs2 = avg_Mabs * avg_Mabs;
-
-            // var_M = avg_M2 - avg_M * avg_M; was this and is prolly wrong
-
-            var_M = avg_M2 - avg_Mabs2;
-            Cv = (var_E) / (N * kb * T * T);
-            chi = (var_M) / (N * kb * T);
             if (i % int(N) == 0 && i != 0) // output only at the end of a mc_cycle
             {
-                file1 << config << std::endl;
+                file1
+                    << config << std::endl;
                 file2 << std::setw(25) << std::setprecision(15)
                       << avg_e << " " << avg_mabs << " " << Cv << " " << chi
                       << std::endl;
