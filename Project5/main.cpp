4c1dd1fa756a12d96969e6bf90c4b0283abe7b70
Adamjakobsen <adam.r.jakobsen@gmail.com>
2022-12-05 16:46:07 +0100

    All done

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -4,125 +4,129 @@
 #include <iomanip>
 #include <armadillo>
 
+int main(int argc, char *argv[])
+{
+    // Declaring variables
+    arma::cx_mat U0;
+    double h, dt, T, x_0, sigma_x, px, y_0, sigma_y, py;
+    std::string slits;
+    double v0 = std::pow(10, 10) * 1.; // potential
 
+    // Input filename from command line
+    std::string input_filename = argv[1];
+    std::string path_input = "./input/";
 
-int main(int argc ,char *argv[]){
-//Declaring variables
-arma::cx_mat U0;
-double h,dt,T,x_0,sigma_x,px,y_0,sigma_y,py;
-std::string slits;
-double v0=std::pow(10,10)*1.;// potential
+    // create filestream to read in inputs from file
+    std::ifstream infile;
+    infile.open(path_input + input_filename + ".txt");
 
-//Input filename from command line
-std::string input_filename=argv[1];
+    if (!infile.is_open())
+    {
+        std::cout << "Error opening file" << std::endl;
+        return 1;
+    }
 
-//create filestream to read in inputs from file
-std::ifstream infile;
-infile.open(input_filename);
+    // read in inputs from file
 
+    std::string line;
 
-if(!infile.is_open()){
-std::cout<<"Error opening file"<<std::endl;
-return 1;
+    while (std::getline(infile, line))
+    {
+        // loop through lines and sett corect variables. name and value separated by "="
+        std::string name = line.substr(0, line.find("="));
+        std::string value = line.substr(line.find("=") + 1);
+        if (name == "h")
+        {
+            h = std::stod(value);
+        }
+        else if (name == "dt")
+        {
+            dt = std::stod(value);
+        }
+        else if (name == "T")
+        {
+            T = std::stod(value);
+        }
+        else if (name == "x_0")
+        {
+            x_0 = std::stod(value);
+        }
+        else if (name == "sigma_x")
+        {
+            sigma_x = std::stod(value);
+        }
+        else if (name == "px")
+        {
+            px = std::stod(value);
+        }
+        else if (name == "y_0")
+        {
+            y_0 = std::stod(value);
+        }
+        else if (name == "sigma_y")
+        {
+            sigma_y = std::stod(value);
+        }
+        else if (name == "py")
+        {
+            py = std::stod(value);
+        }
+        else if (name == "slits")
+        {
+            slits = value;
+        }
+        else
+        {
+            std::cout << "Error reading file" << std::endl;
+            return 1;
+        }
+    }
+    // print variables
+    std::cout << "Running configurat from " << input_filename + " :" << std::endl;
+    std::cout << "h=" << h << std::endl;
+    std::cout << "dt=" << dt << std::endl;
+    std::cout << "T=" << T << std::endl;
+    std::cout << "x_0=" << x_0 << std::endl;
+    std::cout << "sigma_x=" << sigma_x << std::endl;
+    std::cout << "px=" << px << std::endl;
+    std::cout << "y_0=" << y_0 << std::endl;
+    std::cout << "sigma_y=" << sigma_y << std::endl;
+    std::cout << "py=" << py << std::endl;
+    std::cout << "slits=" << slits << std::endl;
+    std::cout << "" << std::endl;
+
+    int M = int(1.0 / h) + 1; // number of points
+    int N = M - 2;            // number of inner points
+
+    Schrodinger sc = Schrodinger(v0, h, dt, M);
+
+    sc.set_potential(slits);
+
+    sc.set_A_B();
+
+    U0 = sc.initialise_state(sigma_x, sigma_y, x_0, y_0, px, py);
+    std::cout << "State initialised" << std::endl;
+    std::cout << "" << std::endl;
+
+    double T_dt = T / dt;
+
+    int n_timesteps = (int)(T / dt);
+
+    // Define matrix: holds the state as vector at each timestep
+    arma::cx_mat U = arma::cx_mat(N * N, n_timesteps);
+
+    U.col(0) = sc.get_u_vec(U0);
+
+    for (int i = 1; i < n_timesteps; i++)
+    {
+        U.col(i) = sc.evolve(U.col(i - 1));
+    }
+    // Save U to file
+    string path = "./data/";
+    string filename = "U_" + input_filename + ".bin";
+    U.save(path + filename);
+    std::cout << "data saved to " << path + filename << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "======================================================================" << std::endl;
+    return 0;
 }
-
-//read in inputs from file
-
-std::string line;
-
-while(std::getline(infile,line)){
-    //loop through lines and sett corect variables. name and value separated by "="
-    std::string name=line.substr(0,line.find("="));
-    std::string value=line.substr(line.find("=")+1);
-    if(name=="h"){
-        h=std::stod(value);
-    }
-    else if(name=="dt"){
-        dt=std::stod(value);
-    }
-    else if(name=="T"){
-        T=std::stod(value);
-    }
-    else if(name=="x_0"){
-        x_0=std::stod(value);
-    }
-    else if(name=="sigma_x"){
-        sigma_x=std::stod(value);
-    }
-    else if(name=="px"){
-        px=std::stod(value);
-    }
-    else if(name=="y_0"){
-        y_0=std::stod(value);
-    }
-    else if(name=="sigma_y"){
-        sigma_y=std::stod(value);
-    }
-    else if(name=="py"){
-        py=std::stod(value);
-    }
-    else if(name=="slits"){
-        slits=value;
-    }
-    else{
-        std::cout<<"Error reading file"<<std::endl;
-return 1;
-}
-}
-//print variables
-std::cout<<"h="<<h<<std::endl;
-std::cout<<"dt="<<dt<<std::endl;
-std::cout<<"T="<<T<<std::endl;
-std::cout<<"x_0="<<x_0<<std::endl;
-std::cout<<"sigma_x="<<sigma_x<<std::endl;
-std::cout<<"px="<<px<<std::endl;
-std::cout<<"y_0="<<y_0<<std::endl;
-std::cout<<"sigma_y="<<sigma_y<<std::endl;
-std::cout<<"py="<<py<<std::endl;
-std::cout<<"slits="<<slits<<std::endl;
-
-
-
-int M = int(1.0/h)+1;// number of points
-int N = M-2; // number of inner points
-
-
-
-
-Schrodinger sc=Schrodinger(v0,h,dt,M);
-
-
-
-
-
-
-sc.set_potential(slits);
-
-sc.set_A_B();
-
-U0=sc.initialise_state(sigma_x,sigma_y,x_0,y_0,px,py);
-std::cout << "State initialised" << std::endl;
-
-double T_dt=T/dt;
-
-int n_timesteps = (int)(T/dt);
-
-arma::cx_mat U = arma::cx_mat(N*N,n_timesteps);
-//print shape of U
-std::cout << "U shape: " << U.n_rows << " " << U.n_cols << std::endl;
-
-
-
-U.col(0)=sc.get_u_vec(U0);
-std::cout << "U(0) set" << std::endl;
-
-for (int i=1;i<n_timesteps;i++){
-    U.col(i)=sc.evolve(U.col(i-1));
-}
-string path="./data/";
-string filename="U_"+slits+".bin";
-U.save(path+filename);
-
-return 0;
-}
-
--- Project5/src/schrodinger.cpp
+++ Project5/src/schrodinger.cpp
@@ -5,211 +5,189 @@
 #include <complex>
 using namespace std;
 
-
-
-
-
-Schrodinger::Schrodinger(double v0_in, double h_in,double dt_in,int M_in)
+Schrodinger::Schrodinger(double v0_in, double h_in, double dt_in, int M_in)
 {
     v0 = v0_in;
     h = h_in;
     dt = dt_in;
     M = M_in;
-    
-    arma::cx_mat V ;
-    arma::mat A ;
-    arma::mat B ;
+
+    arma::cx_mat V;
+    arma::mat A;
+    arma::mat B;
 }
 
-int Schrodinger::ij_k(int i, int j) {
+int Schrodinger::ij_k(int i, int j)
+{
     // converts 2D indices to 1D
-    
-    return i*(M-2) + j;
+
+    return i * (M - 2) + j;
 }
-int Schrodinger::pos_to_idx(double pos) {
-    //positon to index
-    int  index=int(pos/h);
-    
-    
+int Schrodinger::pos_to_idx(double pos)
+{
+    // positon to index
+    int index = int(pos / h);
+
     return index;
 }
 
 void Schrodinger::set_potential(string slits)
 {
-    
+
     // set potential
-    
-    V = arma::zeros<arma::cx_mat>(M,M);
-    int width_walls=pos_to_idx(0.02);
-    V.cols(0,width_walls).fill(v0);
-    
-    V.cols(M-1-width_walls ,M-1)= arma::ones<arma::cx_mat>(M,width_walls+1)*v0;
-    V.rows(0,width_walls) = arma::ones<arma::cx_mat>(width_walls+1,M)*v0;
-    V.rows(M-1-width_walls ,M-1)= arma::ones<arma::cx_mat>(width_walls+1,M)*v0;
-    cout<<"v0:"<<v0 <<endl;
-    V.rows(pos_to_idx(0.5-0.01),pos_to_idx(0.5+0.01))=arma::ones<arma::cx_mat>(width_walls+1,M)*v0;
 
+    V = arma::zeros<arma::cx_mat>(M, M);
+    int width_walls = pos_to_idx(0.02);
+    V.cols(0, width_walls).fill(v0);
 
-    int start_x=pos_to_idx(0.5-0.01);
-    int end_x=pos_to_idx(0.5+0.01);
-    
-    if (slits=="double")
-    
-    {   
-        int left_start_y=pos_to_idx(0.5-0.075);
-        int left_end_y=pos_to_idx(0.5-0.025);
+    V.cols(M - 1 - width_walls, M - 1) = arma::ones<arma::cx_mat>(M, width_walls + 1) * v0;
+    V.rows(0, width_walls) = arma::ones<arma::cx_mat>(width_walls + 1, M) * v0;
+    V.rows(M - 1 - width_walls, M - 1) = arma::ones<arma::cx_mat>(width_walls + 1, M) * v0;
 
-        int right_start_y=pos_to_idx(0.5+0.025);
-        int right_end_y=pos_to_idx(0.5+0.075)+1;
-
-        V.submat(arma::span(start_x,end_x), arma::span(left_start_y,left_end_y))    =arma::zeros<arma::cx_mat>(end_x-start_x+1,left_end_y-left_start_y+1); 
-        V.submat(arma::span(start_x,end_x), arma::span(right_start_y,right_end_y))  =arma::zeros<arma::cx_mat>(end_x-start_x+1,right_end_y-right_start_y+1); 
-        
-        
-    }
-    
-    //make slit in middle wall
-    if (slits=="single")
-    {   
-        int middle_start_y=pos_to_idx(0.5-0.025);
-        int middle_end_y=pos_to_idx(0.5+0.025);
-        V.submat(arma::span(start_x,end_x), arma::span(middle_start_y,middle_end_y))=arma::zeros<arma::cx_mat>(end_x-start_x+1,middle_end_y-middle_start_y+1); 
-
-        
-        
-
-    
-    }
-
-    
-    //Triple slit
-    if (slits=="triple")
+    if (slits != "none")
     {
-    
-
-    int middle_start_y=pos_to_idx(0.5-0.025);
-    int middle_end_y=pos_to_idx(0.5+0.025);
-
-    int left_start_y=pos_to_idx(0.5-0.075-0.05);
-    int left_end_y=pos_to_idx(0.5-0.075)+1;
-
-    int right_start_y=pos_to_idx(0.5+0.075);
-    int right_end_y=pos_to_idx(0.5+0.075+0.05);
-
-    V.submat(arma::span(start_x,end_x), arma::span(middle_start_y,middle_end_y))=arma::zeros<arma::cx_mat>(end_x-start_x+1,middle_end_y-middle_start_y+1); 
-    V.submat(arma::span(start_x,end_x), arma::span(left_start_y,left_end_y))    =arma::zeros<arma::cx_mat>(end_x-start_x+1,left_end_y-left_start_y+1); 
-    V.submat(arma::span(start_x,end_x), arma::span(right_start_y,right_end_y))  =arma::zeros<arma::cx_mat>(end_x-start_x+1,right_end_y-right_start_y+1); 
+        V.rows(pos_to_idx(0.5 - 0.01), pos_to_idx(0.5 + 0.01)) = arma::ones<arma::cx_mat>(width_walls + 1, M) * v0;
     }
-    
 
-    
-    
+    int start_x = pos_to_idx(0.5 - 0.01);
+    int end_x = pos_to_idx(0.5 + 0.01);
 
+    if (slits == "double")
 
+    {
+        int left_start_y = pos_to_idx(0.5 - 0.075);
+        int left_end_y = pos_to_idx(0.5 - 0.025);
 
-    V.save("./data/V.bin");
-    
-        
-    
+        int right_start_y = pos_to_idx(0.5 + 0.025);
+        int right_end_y = pos_to_idx(0.5 + 0.075) + 1;
 
+        V.submat(arma::span(start_x, end_x), arma::span(left_start_y, left_end_y)) = arma::zeros<arma::cx_mat>(end_x - start_x + 1, left_end_y - left_start_y + 1);
+        V.submat(arma::span(start_x, end_x), arma::span(right_start_y, right_end_y)) = arma::zeros<arma::cx_mat>(end_x - start_x + 1, right_end_y - right_start_y + 1);
+    }
+
+    // make slit in middle wall
+    if (slits == "single")
+    {
+        int middle_start_y = pos_to_idx(0.5 - 0.025);
+        int middle_end_y = pos_to_idx(0.5 + 0.025);
+        V.submat(arma::span(start_x, end_x), arma::span(middle_start_y, middle_end_y)) = arma::zeros<arma::cx_mat>(end_x - start_x + 1, middle_end_y - middle_start_y + 1);
+    }
+
+    // Triple slit
+    if (slits == "triple")
+    {
+
+        int middle_start_y = pos_to_idx(0.5 - 0.025);
+        int middle_end_y = pos_to_idx(0.5 + 0.025);
+
+        int left_start_y = pos_to_idx(0.5 - 0.075 - 0.05);
+        int left_end_y = pos_to_idx(0.5 - 0.075) + 1;
+
+        int right_start_y = pos_to_idx(0.5 + 0.075);
+        int right_end_y = pos_to_idx(0.5 + 0.075 + 0.05);
+
+        V.submat(arma::span(start_x, end_x), arma::span(middle_start_y, middle_end_y)) = arma::zeros<arma::cx_mat>(end_x - start_x + 1, middle_end_y - middle_start_y + 1);
+        V.submat(arma::span(start_x, end_x), arma::span(left_start_y, left_end_y)) = arma::zeros<arma::cx_mat>(end_x - start_x + 1, left_end_y - left_start_y + 1);
+        V.submat(arma::span(start_x, end_x), arma::span(right_start_y, right_end_y)) = arma::zeros<arma::cx_mat>(end_x - start_x + 1, right_end_y - right_start_y + 1);
+    }
+
+    V.save("./data/V" + slits + ".bin");
 }
 
 void Schrodinger::set_A_B()
-{   
-    
-    cout<<"set A_B"<< (M-2)<<"M:"<<M <<endl;
-    A = arma::sp_cx_mat((M-2)*(M-2),(M-2)*(M-2));
-    B = arma::sp_cx_mat((M-2)*(M-2),(M-2)*(M-2));
+{
 
-    
+    A = arma::sp_cx_mat((M - 2) * (M - 2), (M - 2) * (M - 2));
+    B = arma::sp_cx_mat((M - 2) * (M - 2), (M - 2) * (M - 2));
+
     complex<double> ak;
     complex<double> bk;
-    complex<double> r=1i*dt/(2*h*h);
-    
+    complex<double> r = 1i * dt / (2 * h * h);
+
     int i;
     int j;
-    for (int k=0; k<(M-2)*(M-2); k++){
-        
-        i=k/(M-2);
-        j=k%(M-2);
-        ak = 1. + 4.*r +1i*dt*V(i,j)/2.;
-        bk = 1. - 4.*r -1i*dt*V(i,j)/2.;
-        A(k,k)=ak;
-        B(k,k)=bk;
-         
-        if (i!=0){
-            A(k , ij_k(i-1,j))=-r;
-            A(ij_k(i-1,j) , k)=-r;
+    for (int k = 0; k < (M - 2) * (M - 2); k++)
+    {
 
-            B(k , ij_k(i-1,j))=r;
-            B(ij_k(i-1,j) , k)=r;
+        i = k / (M - 2);
+        j = k % (M - 2);
+
+        ak = 1. + 4. * r + 1i * dt * V(i, j) / 2.;
+        bk = 1. - 4. * r - 1i * dt * V(i, j) / 2.;
+        A(k, k) = ak;
+        B(k, k) = bk;
+
+        if (i != 0)
+        { // lowermost diagonal
+            A(k, ij_k(i - 1, j)) = -r;
+            // uppermost diagonal
+            A(ij_k(i - 1, j), k) = -r;
+
+            // lowermost diagonal
+            B(k, ij_k(i - 1, j)) = r;
+            // uppermost diagonal
+            B(ij_k(i - 1, j), k) = r;
         }
-        if (j!=0){
-            A(k , ij_k(i,j-1))=-r;
-            A(ij_k(i,j-1) , k)=-r;
+        if (j != 0)
+        { //"Inner" diagonals
+            A(k, ij_k(i, j - 1)) = -r;
+            A(ij_k(i, j - 1), k) = -r;
 
-            B(k , ij_k(i,j-1))=r;
-            B(ij_k(i,j-1) , k)=r;
-
+            B(k, ij_k(i, j - 1)) = r;
+            B(ij_k(i, j - 1), k) = r;
         }
-
-        
     }
-    A.save("./data/A.bin");
-    cout<<"A set"<<endl;
-    
-             
 }
 
-arma::cx_vec Schrodinger::evolve(arma::cx_vec u_vec){
+arma::cx_vec Schrodinger::evolve(arma::cx_vec u_vec)
+{
     arma::superlu_opts opts;
     opts.symmetric = true;
     // evolve wavefunction
     arma::cx_vec u_vec_new;
     u_vec_new = arma::zeros<arma::cx_vec>(u_vec.n_elem);
-    u_vec_new = arma::spsolve(A,B*u_vec, "superlu",opts);
+    u_vec_new = arma::spsolve(A, B * u_vec, "superlu", opts);
     return u_vec_new;
 }
 
-complex<double> Schrodinger::gaussian(double x, double y, double sigma_x, double sigma_y, double x0, double y0,double px,double py){
-    // gaussian wavepacke
+complex<double> Schrodinger::gaussian(double x, double y, double sigma_x, double sigma_y, double x0, double y0, double px, double py)
+{
+    // gaussian wavepacket
     complex<double> psi;
-    psi = exp(-pow(x-x0,2)/(2*pow(sigma_x,2)) - pow(y-y0,2)/(2*pow(sigma_y,2)))*exp(1i*(px*(x-x0) + py*(y-y0)));
+    psi = exp(-pow(x - x0, 2) / (2 * pow(sigma_x, 2)) - pow(y - y0, 2) / (2 * pow(sigma_y, 2))) * exp(1i * (px * (x - x0) + py * (y - y0)));
     return psi;
 }
 
-arma::cx_mat Schrodinger::initialise_state(double sigma_x, double sigma_y, double x0, double y0,double px,double py){
+arma::cx_mat Schrodinger::initialise_state(double sigma_x, double sigma_y, double x0, double y0, double px, double py)
+{
     // initialize wavefunction
     arma::cx_mat U;
-    U = arma::zeros<arma::cx_mat>(M-2,M-2);
-    for (int i=0;i<M-2;i++){
-        for (int j=0;j<M-2;j++){
-           
-            U(i,j) = gaussian(i*h,j*h,sigma_x,sigma_y,x0,y0,px,py);
+    U = arma::zeros<arma::cx_mat>(M - 2, M - 2);
+    for (int i = 0; i < M - 2; i++)
+    {
+        for (int j = 0; j < M - 2; j++)
+        {
+
+            U(i, j) = gaussian(i * h, j * h, sigma_x, sigma_y, x0, y0, px, py);
         }
     }
 
-    /* //Boundary conditions
-    U.row(0).fill(0);
-    U.row(M-1).fill(0);
-    U.col(0).fill(0);
-    cout<<"0 col set"<<endl;
-    U.col(M-1).fill(0);
-    cout<<"M-1 col set"<<endl;
-     */
-    //Normalize
-    U = U/sqrt(arma::accu(arma::conj(U)%U));
-    cout << "U set and normalised" << endl;
+    // Normalize
+    U = U / sqrt(arma::accu(arma::conj(U) % U));
+
     return U;
 }
 
-arma::cx_vec Schrodinger::get_u_vec(arma::cx_mat U){
+arma::cx_vec Schrodinger::get_u_vec(arma::cx_mat U)
+{
     // convert 2D matrix to 1D vector
     arma::cx_vec u_vec;
-    u_vec = arma::zeros<arma::cx_vec>((M-2)*(M-2));
-    for (int i=0;i<M-2;i++){
-        for (int j=0;j<M-2;j++){
-            u_vec(ij_k(i,j)) = U(i,j);
+    u_vec = arma::zeros<arma::cx_vec>((M - 2) * (M - 2));
+    for (int i = 0; i < M - 2; i++)
+    {
+        for (int j = 0; j < M - 2; j++)
+        {
+            u_vec(ij_k(i, j)) = U(i, j);
         }
     }
     return u_vec;
